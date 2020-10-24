44e4c5a938a8e7a6844a95448adc25bba5e940f6
Leonard Laszlo <laslaul@yahoo.com>
2020-10-24 10:42:24 +0200

    Immprove bash style.

--- hungary.sh
+++ hungary.sh
@@ -8,7 +8,7 @@
 match() {
 	regexp='"name":"'$2'","longitude":\-?[0-9\.]+,"latitude":\-?[0-9\.]+,"value":(\-?[0-9\.]+),';
 	[[ $1 =~ $regexp ]];
-	echo ${BASH_REMATCH[1]}°C;
+	echo "${BASH_REMATCH[1]}"°C;
 }
 
 rawdata=$(curl -s "https://koponyeg.hu/elorejelzes/Budapest?m=weathermap&a=temperaturemap&_ajax=1");
@@ -31,22 +31,22 @@ printf "\033[0;32m                                                    .:NN.     
 printf "\033[0;32m                                           :yNds/-+NMmh.               '-'    sMMN/ \033[0m\n"
 printf "\033[0;32m                                   .-////osNN-+dNMMm'                           'dMhdy' \033[0m\n"
 printf "\033[0;32m             :hmds.              'yMh+osyo++.    +/                              '::sM- \033[0m\n"
-printf "\033[0;32m            :mM/:hMs-            .Md                \033[1;33m$mh\033[0;32m                            +mM/ \033[0m\n"
+printf "\033[0;32m            :mM/:hMs-            .Md                \033[1;33m%s\033[0;32m                            +mM/ \033[0m\n" "$mh"
 printf "\033[0;32m      .oo/../Md   :hNm+++oo+//ossyM/                                         .oyNmdMh- \033[0m\n"
 printf "\033[0;32m     hMMmMMMNmh      /osy++oss+///.                                        /sNmo+--' \033[0m\n"
-printf "\033[0;32m     .+sMN-    \033[1;33m$gh\033[0;32m                                                        -mMd. \033[0m\n"
-printf "\033[0;32m     -/oMN'                        \033[1;33m$bh\033[0;32m                                    dM: \033[0m\n"
-printf "\033[0;32m     dMh+/                                                    \033[1;33m$dh\033[0;32m       'mNd. \033[0m\n"
+printf "\033[0;32m     .+sMN-    \033[1;33m%s\033[0;32m                                                        -mMd. \033[0m\n" "$gh"
+printf "\033[0;32m     -/oMN'                        \033[1;33m%s\033[0;32m                                    dM: \033[0m\n" "$bh"
+printf "\033[0;32m     dMh+/                                                    \033[1;33m%s\033[0;32m       'mNd. \033[0m\n" "$dh"
 printf "\033[0;32m     mM/                                                                hM/ \033[0m\n"
 printf "\033[0;32m     hMm                                                              .yMy \033[0m\n"
 printf "\033[0;32m   ohmMm                                                             +MN: \033[0m\n"
-printf "\033[0;32m :NMm/:.                \033[1;33m$zh\033[0;32m                                         .NMo \033[0m\n"
+printf "\033[0;32m :NMm/:.                \033[1;33m%s\033[0;32m                                         .NMo \033[0m\n" "$zh"
 printf "\033[0;32m /syMN'                                                            'NN/ \033[0m\n"
 printf "\033[0;32m   .dMy                                                           -dMd \033[0m\n"
 printf "\033[0;32m    -dNo                                                         yMm+ \033[0m\n"
 printf "\033[0;32m      sMd+                                                      'MM- \033[0m\n"
-printf "\033[0;32m       '+mMh-                                    \033[1;33m$sh\033[0;32m     -   'mMh' \033[0m\n"
-printf "\033[0;32m          :NMo.           \033[1;33m$ph\033[0;32m                       . oMMMmMm+ \033[0m\n"
+printf "\033[0;32m       '+mMh-                                    \033[1;33m%s\033[0;32m     -   'mMh' \033[0m\n" "$sh"
+printf "\033[0;32m          :NMo.           \033[1;33m%s\033[0;32m                       . oMMMmMm+ \033[0m\n" "$ph"
 printf "\033[0;32m           '+hMd-                          yMNNmmMMMmdNNMNNm-'.- \033[0m\n"
 printf "\033[0;32m              /NN/                 so'sh+hNNo ':- -.:/.-'-- \033[0m\n"
 printf "\033[0;32m               -dMNdmm+         .syMNNMyoo: \033[0m\n"
